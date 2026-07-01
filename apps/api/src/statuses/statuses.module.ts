6ddd5d48f1ac09ae5d6bca0b3017d18e0069d02b
Emre Colakoglu <emrinho@gmail.com>
2026-07-01 13:28:47 +0300

    feat(api): add EventsModule to StatusesModule imports

--- apps/api/src/statuses/statuses.module.ts
+++ apps/api/src/statuses/statuses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StatusesController } from './statuses.controller';
 import { StatusesService } from './statuses.service';
+import { EventsModule } from '../events/events.module';
 
 @Module({
+  imports: [EventsModule],
   controllers: [StatusesController],
   providers: [StatusesService],
   exports: [StatusesService],
